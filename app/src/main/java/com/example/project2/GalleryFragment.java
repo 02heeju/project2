af4f108662c92a8397d106b90e62148007a3cb77
02heeju <02heeju@kaist.ac.kr>
2020-07-21 06:52:49 +0900

    phone -> cloud 버튼 클릭시 서버에 이미지 업로드

--- app/src/main/java/com/example/project2/GalleryFragment.java
+++ app/src/main/java/com/example/project2/GalleryFragment.java
@@ -1,13 +1,9 @@
 package com.example.project2;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,31 +13,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.example.project2.Retrofit.IMyService;
-import com.example.project2.Retrofit.RetrofitClient;
-
-import java.io.File;
-import java.io.InputStream;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Retrofit;
 
 
 
@@ -74,9 +56,15 @@ public class GalleryFragment extends Fragment
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), 3);
         mThumbnailRecyclerView.setLayoutManager(gridLayoutManager);
         mMediaStoreAdapter = new MediaStoreAdapter(this.getActivity(), this, user_name, getActivity());
-        mThumbnailRecyclerView.setAdapter(mMediaStoreAdapter);
 
-        checkReadExternalStoragePermission();
+        Button toServer_button = view.findViewById(R.id.toServer_button);
+        toServer_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mThumbnailRecyclerView.setAdapter(mMediaStoreAdapter);
+                checkReadExternalStoragePermission();
+            }
+        });
 
         return view;
     }
